66a003d15a4473fe89a7bce9ac2a6f25bccd8525
Melinda <melinda.sava.td@gmail.com>
2020-11-20 18:08:54 +0100

    first variant of new user function working in console

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-var quill = new Quill('#editor', {
-    theme: 'snow'
-  });
+// var quill = new Quill('#editor', {
+//    theme: 'snow'
+//   });
+
+
+  //new user redirect
+  let pageVisits = localStorage.getItem('pageVisits'); 
+  console.log(pageVisits);
+  if (pageVisits == null) {
+    pageVisits = 0;
+  }
+
+  function redirectToInfo(){
+    window.location = "intro.html";
+  }
+
+  function redirectNewUser(){
+   
+   
+   if (pageVisits == 0) {
+      pageVisits += 1;
+      localStorage.setItem("pageVisits", pageVisits);
+      redirectToInfo();
+    } else {
+      pageVisits += 1;
+      localStorage.setItem("pageVisits", pageVisits);
+    }
+    
+  }
