1e05e2f0c6493d31b6358c243112bee81896f9a7
Melinda <melinda.sava.td@gmail.com>
2020-11-25 00:35:14 +0100

    managed to save notes created in the editor and make title mandatory for saving

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let newNoteButton = document.querySelector(".new-note-button")
-let titleInputContainer = document.querySelector(".titel-input-container")
-let submitTitleBtn = document.querySelector("#submit-title-button")
-let titleInputValue = document.querySelector("#input-title").value
+// let newNoteButton = document.querySelector(".new-note-button")
+// let titleInputContainer = document.querySelector(".titel-input-container")
+// let submitTitleBtn = document.querySelector("#submit-title-button")
+// let titleInputValue = document.querySelector("#input-title").value
 
-//Event listeners
-newNoteButton.addEventListener("click", createNewNote)
+
+// //Event listeners
+// newNoteButton.addEventListener("click", createNewNote)
 
 //Global variable for click checks
 let clicked = 0;
@@ -15,53 +16,52 @@ var quill = new Quill('#editor', {
   });
 
 //Function for opening the input field for creating a new note
-function createNewNote() {
-  //First checks the global check variable
-  if(clicked < 1){
-    //Removes the "hidden" css class from the input field container
-    titleInputContainer.classList.remove("hidden")
-    //Adds the "hidden" css class to the entire editor container to only show the input field container
-    document.querySelector(".toolbar-and-editor-container").classList.add("hidden")
-    //Removes the first child (previous title) so a new first child can be used
-    document.querySelector(".ql-editor").removeChild(document.querySelector(".ql-editor").childNodes[0])
-    //Adds value to the global variable so the "Create new note" button does nothing while the input container is used
-    clicked ++;
-  } 
-}
+// function createNewNote() {
+//   //First checks the global check variable
+//   if(clicked < 1){
+//     //Removes the "hidden" css class from the input field container
+//     titleInputContainer.classList.remove("hidden")
+//     //Adds the "hidden" css class to the entire editor container to only show the input field container
+//     document.querySelector(".toolbar-and-editor-container").classList.add("hidden")
+//     //Removes the first child (previous title) so a new first child can be used
+//     document.querySelector(".ql-editor").removeChild(document.querySelector(".ql-editor").childNodes[0])
+//     //Adds value to the global variable so the "Create new note" button does nothing while the input container is used
+//     clicked ++;
+//   } 
+// }
 
-//Function for submiting the input value as the title for the editor
-function submitTitle() {
-  //Sets the value of the input field to a vatiable
-  let inputValue = document.querySelector("#input-title").value;
+// //Function for submiting the input value as the title for the editor
+// function submitTitle() {
+//   //Sets the value of the input field to a vatiable
+//   let inputValue = document.querySelector("#input-title").value;
 
-  //Creates a h1 element and sets it to a variable
-  let h = document.createElement("h1");
+//   //Creates a h1 element and sets it to a variable
+//   let h = document.createElement("h1");
 
-  //Creates text that uses the value of the input field and sets it to a variable
-  var t = document.createTextNode(inputValue);
+//   //Creates text that uses the value of the input field and sets it to a variable
+//   var t = document.createTextNode(inputValue);
 
-  //Appends the text with the value to the h1 element
-  h.appendChild(t);
+//   //Appends the text with the value to the h1 element
+//   h.appendChild(t);
 
-  //"Getting" the element that is the parent were we want to place the title
-  let parentElement = document.querySelector(".ql-editor")
-  //"Getting" the current first child element (an empty "p" tag that comes with the editor when created)
-  let theFirstChild = parentElement.firstChild
-  //Places the new title before the empty "p" tag
-  parentElement.insertBefore(h, theFirstChild)
-  //Resets the value of the input field to blank
-  document.querySelector("#input-title").value = ""
-  //Adds the "hidden" class to the input container
-  titleInputContainer.classList.add("hidden")
-  //Removes the "hidden" class from the editor container
-  document.querySelector(".toolbar-and-editor-container").classList.remove("hidden")
-  //Removes value from the global "check" variable so the "Create new note" button can be used again
-  clicked --;
-}
+//   //"Getting" the element that is the parent were we want to place the title
+//   let parentElement = document.querySelector(".ql-editor")
+//   //"Getting" the current first child element (an empty "p" tag that comes with the editor when created)
+//   let theFirstChild = parentElement.firstChild
+//   //Places the new title before the empty "p" tag
+//   parentElement.insertBefore(h, theFirstChild)
+//   //Resets the value of the input field to blank
+//   document.querySelector("#input-title").value = ""
+//   //Adds the "hidden" class to the input container
+//   titleInputContainer.classList.add("hidden")
+//   //Removes the "hidden" class from the editor container
+//   document.querySelector(".toolbar-and-editor-container").classList.remove("hidden")
+//   //Removes value from the global "check" variable so the "Create new note" button can be used again
+//   clicked --;
+// }
 
   //new user redirect
   let pageVisits = JSON.parse(localStorage.getItem('pageVisits')); 
-  console.log(pageVisits);
 
   function redirectToInfo(){
     window.location = "intro.html";
@@ -74,6 +74,33 @@ function submitTitle() {
       redirectToInfo();
     } 
   }
+
+
+  //create note
+  let editingField = document.querySelector(".ql-editor");
+  let notesListContainer = document.querySelector('.saved-notes-list');
+  let saveNoteBtn = document.querySelector('.save-note-btn');
+
+
+  saveNoteBtn.onclick = function saveNote(){
+    let firstElement = editingField.firstChild;
+
+    if (firstElement.tagName === 'H1' && firstElement.textContent.trim() != "") {
+      notesListContainer.innerHTML += `<li class="note">${editingField.innerHTML}</li>`;
+
+
+    } else alert("Please add a heading to your note, it will act as the note\'s title")
+  }
+
+  // editingField.addEventListener(onkeyup, () => {
+  //   console.log("in listener");
+    
+  //   // let note = document.createElement("li");
+
+
+
+  //   // notesListContainer.appendChild(note);
+  // })
 
 
   /* ADDITIONAL FUNCTIONS NOT IMPLEMENTED */
